630b0ad07db975744b6d97b8a32edac2d3f1bd95
[504]brandon <97409501+504brandon@users.noreply.github.com>
2022-06-24 23:12:07 -0500

    made it so songs like man-on-stilts is now man on stilts also added vs noob

---
+++ vs noob/characters/noob.hx
@@ -0,0 +1,16 @@
+// create your character in this lmao
+function createCharacter() {
+    character.frames = Paths.getSparrowAtlas("characters/Noob");
+    
+    character.quickAnimAdd('idle', 'Noob Idle');
+    character.quickAnimAdd('singUP', 'Noob Up');
+    character.quickAnimAdd('singRIGHT', 'Noob Right');
+    character.quickAnimAdd('singDOWN', 'Noob Down');
+    character.quickAnimAdd('singLEFT', 'Noob Left');
+
+    character.loadOffsetFile('noob');
+
+    character.playAnim('idle');
+
+    character.health_color = 0xFFF3CF0A;
+}
